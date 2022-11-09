e1e85fb93c388f8cd875c632daf5872bd7ee043f
afandy <afandy.lamusu@live.co.uk>
2022-11-09 20:55:30 +0700

    update validation

--- apps/pelaporan-rest/routes/kegiatan-umum/index.js
+++ apps/pelaporan-rest/routes/kegiatan-umum/index.js
@@ -1,11 +1,83 @@
 "use strict";
 
 const { getSchema, postSchema, deleteSchema, putSchema } = require("./schema");
-const { ValidateFormPelaporanKegiatan } = require("./pelaporan-kegiatan.shared");
-
 
 module.exports = async function(server, opts) {
   const DbSet = () => server.models.LaporanKegiatan;
+
+  const GetJenisKegiatanById = async (id) => {
+    const res = await server.rest.masterdata().get(`jenis-kegiatan/?$filter=id eq ${id}`).json();
+    if (res.data != null && res.data.length > 0) return res.data[0];
+    else return null;
+  };
+
+  const GetJenisPasalById = async (id) => {
+    const res = await server.rest.masterdata().get(`jenis-perda-perkada/?$filter=id eq ${id}`).json();
+    if (res.data != null && res.data.length > 0) return res.data[0];
+    else return null;
+  };
+
+  const AddModeErrorReference = (errors, prop) => {
+    errors.push({
+      "instancePath": `/${prop}`,
+      "schemaPath": `#/properties/${prop}/reference`,
+      "keyword": "reference",
+      "params": {},
+      "message": `invalid reference of '${prop}'`
+    });
+  };
+
+  const ValidateFormPelaporanKegiatan = async (form, allowedJK = [], notAllowedJK= []) => {
+    const errors = [];
+    if (form.kegiatan__jenis_kegiatan_id <= 0)
+      AddModeErrorReference(errors, "kegiatan__jenis_kegiatan_id");
+
+    if (errors.length > 0) return errors;
+
+    const jenisKegiatan = await GetJenisKegiatanById(form.kegiatan__jenis_kegiatan_id);
+
+    if(allowedJK.length > 0) {
+      if (jenisKegiatan == null || !allowedJK.includes(jenisKegiatan.nama))
+        AddModeErrorReference(errors, "kegiatan__jenis_kegiatan_id");
+    }
+
+    if (errors.length > 0) return errors;
+
+    if(notAllowedJK.length > 0) {
+      if (jenisKegiatan == null || notAllowedJK.includes(jenisKegiatan.nama))
+        AddModeErrorReference(errors, "kegiatan__jenis_kegiatan_id");
+    }
+
+    if (errors.length > 0) return errors;
+
+    if(form.tindak_lanjut__administrasi__jenis_pasal_id !== undefined) {
+      if (form.tindak_lanjut__administrasi__jenis_pasal_id <= 0)
+        AddModeErrorReference(errors, "tindak_lanjut__administrasi__jenis_pasal_id");
+
+      if (errors.length > 0) return errors;
+
+      const jenisPasal = await GetJenisPasalById(form.tindak_lanjut__administrasi__jenis_pasal_id)
+      if(jenisPasal == null)
+        AddModeErrorReference(errors, "tindak_lanjut__administrasi__jenis_pasal_id");
+
+      if (errors.length > 0) return errors;
+    }
+
+    if(form.tindak_lanjut__administrasi__penyelesaian_id !== undefined) {
+      if (form.tindak_lanjut__administrasi__penyelesaian_id <= 0)
+        AddModeErrorReference(errors, "tindak_lanjut__administrasi__penyelesaian_id");
+
+      if (errors.length > 0) return errors;
+
+      const jenisPasal = await GetJenisPasalById(form.tindak_lanjut__administrasi__penyelesaian_id)
+      if(jenisPasal == null)
+        AddModeErrorReference(errors, "tindak_lanjut__administrasi__penyelesaian_id");
+
+      if (errors.length > 0) return errors;
+    }
+
+    return errors;
+  };
 
   server.get("/", { schema: getSchema }, async function(request, reply) {
     return await server.odata.replyPaging(request, reply, DbSet());
@@ -16,7 +88,7 @@ module.exports = async function(server, opts) {
       return reply.code(400).send({ success: false, ...request.validationError });
     }
 
-    const errors = await ValidateFormPelaporanKegiatan(server, request.body, [], ["SIDANG TIPIRING", "PENERTIBAN BANGUNAN", "KEGIATAN PPKM","LAPORAN MASYARAKAT","PENERTIBAN MINUMAN BERALKOHOL","PENGAMANAN"]);
+    const errors = await ValidateFormPelaporanKegiatan(request.body, [], ["SIDANG TIPIRING", "PENERTIBAN BANGUNAN", "KEGIATAN PPKM","LAPORAN MASYARAKAT","PENERTIBAN MINUMAN BERALKOHOL","PENGAMANAN"]);
     if (errors.length > 0) {
       return reply.code(400).send({ success: false, statusCode: 400, validation: errors, message: "form invalid" });
     }
