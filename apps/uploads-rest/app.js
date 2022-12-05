9dff8269c70bd1719be4a1c9c7ad929b0d6c83d1
afandy <afandy.lamusu@live.co.uk>
2022-12-05 07:22:39 +0700

    update upload-rest

--- apps/uploads-rest/app.js
+++ apps/uploads-rest/app.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const fileUpload = require('fastify-file-upload')
 const AutoLoad = require('@fastify/autoload')
+const fastifyCron = require('fastify-cron')
 
 // Pass --options via CLI arguments in command to enable these options.
 module.exports.options = {}
@@ -10,7 +11,28 @@ module.exports.options = {}
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
 
-  fastify.register(fileUpload)
+  fastify.register(fileUpload, {
+    useTempFiles: true,
+    limits: { fileSize: 50 * 1024 * 1024 },
+  });
+
+  fastify.register(fastifyCron, {
+    jobs: [
+      {
+        // Only these two properties are required,
+        // the rest is from the node-cron API:
+        // https://github.com/kelektiv/node-cron#api
+        cronTime: '* * * * * *', // Every second
+
+        // Note: the callbacks (onTick & onComplete) take the server
+        // as an argument, as opposed to nothing in the node-cron API:
+        onTick: async server => {
+          console.log('tick')
+        },
+        startWhenReady: true
+      }
+    ]
+  })
 
   // Do not touch the following lines
 
--- apps/uploads-rest/routes/root.js
+++ apps/uploads-rest/routes/root.js
@@ -1,8 +1,17 @@
 'use strict'
 
+const {
+  v1: uuidv1,
+  v4: uuidv4
+} = require('uuid');
+
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
+  })
+
+  fastify.post('/commit/:id', async (req, reply) => {
+    reply.send(req.params.id)
   })
 
   fastify.post('/upload', async function (req, reply) {
@@ -11,11 +20,23 @@ module.exports = async function (fastify, opts) {
     console.log(files)
     let fileArr = []
     for(let key in files){
-      fileArr.push({
-        name: files[key].name,
-        mimetype: files[key].mimetype
-      })
+      if(Array.isArray(files[key])){
+        files[key].forEach((v)=>{
+          fileArr.push({
+            name: v.name,
+            mimetype: v.mimetype,
+            tempPath: v.tempFilePath
+          })
+        })
+      }
+      else {
+        fileArr.push({
+          name: files[key].name,
+          mimetype: files[key].mimetype,
+          tempPath: v.tempFilePath
+        })
+      }
     }
-    reply.send(fileArr)
+    reply.send({id: uuidv4(), files: fileArr})
   })
 }
