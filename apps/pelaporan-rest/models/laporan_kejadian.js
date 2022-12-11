e650d63dcc6d0f0b0b4f3ba7fdd395b1c979912d
afandy <afandy.lamusu@live.co.uk>
2022-12-11 08:26:49 +0700

    update pelaporan-rest update responses laporan-kejadian-*

---
+++ apps/pelaporan-rest/models/laporan_kejadian.js
@@ -0,0 +1,162 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('LaporanKejadian', {
+    id: {
+      autoIncrement: true,
+      autoIncrementIdentity: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    is_deleted: {
+      type: DataTypes.SMALLINT,
+      allowNull: true,
+      defaultValue: 0
+    },
+    deleted_by: {
+      type: DataTypes.STRING(64),
+      allowNull: true
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    created_by: {
+      type: DataTypes.STRING(64),
+      allowNull: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updated_by: {
+      type: DataTypes.STRING(64),
+      allowNull: true
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    kejadian__tanggal: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+    },
+    kejadian__waktu_start: {
+      type: DataTypes.STRING(16),
+      allowNull: true
+    },
+    kejadian__waktu_end: {
+      type: DataTypes.STRING(16),
+      allowNull: true
+    },
+    kejadian__kota_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__kecamatan_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__kelurahan_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__alamat: {
+      type: DataTypes.STRING(64),
+      allowNull: true
+    },
+    kejadian__jenis_kegiatan_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__uraian_kejadian: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    kejadian__jml_personil_satpolpp: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__jml_personil_instansilain: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__ketinggian_air: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__pengungsi: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__pengungsi_kk: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    kejadian__lokasi_penampungan: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    kejadian__lokasi_dapur_umum: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__penanggung_jawab: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__jml_massa: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    tindak__tuntutan: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__sumber_info: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__jenis_kekerasan: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__jenis_bantuan_satpolpp: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__jenis_bantuan_instansiterkait: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__korban_jiwa: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__korban_material: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    tindak__dokumentasi: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'laporan_kejadian',
+    schema: 'public',
+    timestamps: false,
+    freezeTableName: true,
+    createdAt: true,
+    indexes: [
+      {
+        name: "laporan_kejadian_pkey",
+        unique: true,
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
