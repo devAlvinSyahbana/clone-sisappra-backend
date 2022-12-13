e7fde529638f5e497aadae58faccbb9be18bc979
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-12-14 00:35:50 +0700

    add jenis asal laporan dan jenis pengamanan

---
+++ apps/masterdata-rest/models/master_jenis_pengamanan.js
@@ -0,0 +1,62 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('MasterJenisPengamanan', {
+    id: {
+      autoIncrement: true,
+      autoIncrementIdentity: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    is_deleted: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    deleted_by: {
+      type: DataTypes.STRING(64),
+      allowNull: true
+    },
+    created_by: {
+      type: DataTypes.STRING(64),
+      allowNull: true
+    },
+    updated_by: {
+      type: DataTypes.STRING(64),
+      allowNull: true
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    nama: {
+      type: DataTypes.STRING(64),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'master_jenis_pengamanan',
+    schema: 'public',
+    timestamps: false,
+    freezeTableName: true,
+    createdAt: true,
+    indexes: [
+      {
+        name: "master_jenis_pengamanan_pkey",
+        unique: true,
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
