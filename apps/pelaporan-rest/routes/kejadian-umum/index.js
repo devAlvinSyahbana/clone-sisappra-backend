209d6279bf1d4cc761fbbbcd9ae432954aea9811
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-12-14 12:46:30 +0700

    edit dbset laporan kejadian, sebelumnya kegiatan

--- apps/pelaporan-rest/routes/kejadian-umum/index.js
+++ apps/pelaporan-rest/routes/kejadian-umum/index.js
@@ -1,32 +1,32 @@
 'use strict'
 
-const {getSchema, postSchema, deleteSchema, putSchema} = require("./schema");
+const { getSchema, postSchema, deleteSchema, putSchema } = require("./schema");
 
 module.exports = async function (server, opts) {
-    const DbSet = () => server.models.LaporanKegiatan
+    const DbSet = () => server.models.LaporanKejadian
 
     const GetJenisKegiatanById = async (id) => {
         const res = await server.rest.masterdata().get(`jenis-kegiatan/?$filter=id eq ${id}`).json()
-        if(res.data != null && res.data.length > 0) return res.data[0]
+        if (res.data != null && res.data.length > 0) return res.data[0]
     }
 
     server.get('/', { schema: getSchema }, async function (request, reply) {
         return await server.odata.replyPaging(request, reply, DbSet())
     })
 
-    server.post('/', {schema: postSchema, attachValidation: true}, async function (request, reply) {
+    server.post('/', { schema: postSchema, attachValidation: true }, async function (request, reply) {
         if (request.validationError) {
-            return reply.code(400).send({success: false, ...request.validationError})
+            return reply.code(400).send({ success: false, ...request.validationError })
         }
 
         let record = server.entity.track(request.body).markCreated("unknown")
 
         const result = await DbSet().create(record)
 
-        return reply.send({success: true, data: [result.dataValues]})
+        return reply.send({ success: true, data: [result.dataValues] })
     })
 
-    server.delete('/:id', {schema: deleteSchema}, async function (request, reply) {
+    server.delete('/:id', { schema: deleteSchema }, async function (request, reply) {
         const record = await DbSet().findOne({
             where: {
                 id: request.params.id,
@@ -41,7 +41,7 @@ module.exports = async function (server, opts) {
                 statusCode: 400
             })
 
-        if(request.headers['x-mode'] && request.headers['x-mode'] === 'TEST') {
+        if (request.headers['x-mode'] && request.headers['x-mode'] === 'TEST') {
             await DbSet().destroy({
                 where: {
                     id: request.params.id,
@@ -54,10 +54,10 @@ module.exports = async function (server, opts) {
             await record.save()
         }
 
-        return reply.send({success: true})
+        return reply.send({ success: true })
     })
 
-    server.put('/:id', {schema: putSchema, attachValidation: true}, async function (request, reply) {
+    server.put('/:id', { schema: putSchema, attachValidation: true }, async function (request, reply) {
         const record = await DbSet().findOne({
             where: {
                 id: request.params.id,
@@ -65,11 +65,11 @@ module.exports = async function (server, opts) {
             }
         })
 
-        if(record == null)
-            return reply.status(400).send({success: false, message: `ID (${request.params.id}) not found`, statusCode: 400})
+        if (record == null)
+            return reply.status(400).send({ success: false, message: `ID (${request.params.id}) not found`, statusCode: 400 })
 
         if (request.validationError) {
-            return reply.code(400).send({success: false, ...request.validationError})
+            return reply.code(400).send({ success: false, ...request.validationError })
         }
 
         record.set(request.body);
