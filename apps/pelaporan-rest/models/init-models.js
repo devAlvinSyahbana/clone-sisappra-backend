6ac96452601ffeeb9a19f4e22794f46951057849
faskal <hasbullahpoel@gmail.com>
2023-01-05 13:54:20 +0700

    update

--- apps/pelaporan-rest/models/init-models.js
+++ apps/pelaporan-rest/models/init-models.js
@@ -1,12 +1,21 @@
 var DataTypes = require("sequelize").DataTypes;
+var _LaporanKegiatan = require("./laporan_kegiatan");
+var _LaporanKejadian = require("./laporan_kejadian");
 var _LaporanPengawasan = require("./laporan_pengawasan");
+var _LaporanTamuDaerah = require("./laporan_tamu_daerah");
 
 function initModels(sequelize) {
+  var LaporanKegiatan = _LaporanKegiatan(sequelize, DataTypes);
+  var LaporanKejadian = _LaporanKejadian(sequelize, DataTypes);
   var LaporanPengawasan = _LaporanPengawasan(sequelize, DataTypes);
+  var LaporanTamuDaerah = _LaporanTamuDaerah(sequelize, DataTypes);
 
 
   return {
+    LaporanKegiatan,
+    LaporanKejadian,
     LaporanPengawasan,
+    LaporanTamuDaerah,
   };
 }
 module.exports = initModels;
